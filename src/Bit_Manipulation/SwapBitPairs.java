f06db47bd7af63a3c88279deceecf9fd0c7acedc
Jeannie Trinh <jetrinh33@gmail.com>
2015-05-26 09:15:53 -0400

    Add initial SwapBitPairs problem file.

---
+++ src/Bit_Manipulation/SwapBitPairs.java
@@ -0,0 +1,17 @@
+package Bit_Manipulation;
+
+public class SwapBitPairs {
+
+	/**
+	 * Question 5.6
+	 * Write a program to swap odd and even bits in an integer with as few instructions as possible.
+	 * e.g. bit 0 and bit 1 are swapped. Bit 2 and bit 3 are swapped, and so on.
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		
+	}
+	
+	
+
+}
