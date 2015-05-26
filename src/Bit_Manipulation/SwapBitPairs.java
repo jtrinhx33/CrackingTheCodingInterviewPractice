639ad03bb163b2f17394cc212645f00e0b69bda5
Jeannie Trinh <jetrinh33@gmail.com>
2015-05-26 10:05:16 -0400

    Add procedure for swapping bit pairs.

--- src/Bit_Manipulation/SwapBitPairs.java
+++ src/Bit_Manipulation/SwapBitPairs.java
@@ -10,8 +10,33 @@ public class SwapBitPairs {
 	 */
 	public static void main(String[] args) {
 		
+		int num = 145;
+		System.out.println("Input: " + Integer.toBinaryString(num));
+		System.out.println("My solution: " + Integer.toBinaryString(swapEvenOddBits(num)));
+		System.out.println("Book's solution: " + Integer.toBinaryString(swapOddEvenBits(num)));
 	}
 	
+	// Takes 5 instructions if we don't include the step to initialize the masks
+	public static int swapEvenOddBits(int num){
+		//int even_mask = Integer.parseInt("01010101", 2); // This should be 32 bit repeat: Use the one below instead for better readability
+		//int even_mask = Integer.parseInt("10101010", 2); // This should be 32 bit repeat: Use the one below instead for better readability
+		
+		int even_mask = 0x55555555; // 5 = 0101, 4 bits --> 5 x 8 = 32 bits of 1010101010...10 for odd # bits
+		int odd_mask = 0xaaaaaaaa; // A = 1010, 4 bits --> A x 8 = 32 bits of 0101010101...01 for even # bits
+		
+		even_mask &= num;
+		odd_mask &= num;
+		
+		even_mask <<= 1;
+		odd_mask >>= 1;
+		
+		// Combine masks
+		return even_mask | odd_mask;
+	}
 	
+	// Book's solution
+	public static int swapOddEvenBits(int x){
+		return ( ((x & 0xaaaaaaaa) >> 1) | ((x & 0x55555555) << 1) );
+	}
 
 }
